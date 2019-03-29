e57acd2a98a8a61df6ee3dbdd323a6c39834afef
Geoff Nowak <geoffreynowak@gmail.com>
2019-03-29 17:13:01 -0400

    Yarn + installed tachyons

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Card.css';
+
+const Card = () => {
+  return (
+    <div>
+      <img src="" />>
+      <div>
+        <h2>Movie Title</h2>
+        <p>Director</p>
+      </div>
+    </div>
+  )
+}
+export default Card;
